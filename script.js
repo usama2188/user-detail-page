83b83afa9b62916895688a86b3151fe47186e767
usama2188 <65440307+usama2188@users.noreply.github.com>
2021-12-21 22:49:51 +0500

    completed

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
 let i = 0;
+let n
 let arr = [];
 let output_val = ''
 
-//generate user
+//GENERATE USER LIST
 function generateUser() {
     document.getElementById("loginForm").style = "display:none"
     document.getElementById("addUser").style = "display:none";
     document.getElementById("deleteUser").style = "display:none";
     document.getElementById("screen").style = "visibility:visible";
+    if (output_val == '') {
+        document.getElementById("output").innerHTML = "No user found in system"
+    }
 
     for (n = 0; n < i; n++) {
         output_val += n + ")  " + arr[n].userName + "<br>"
         document.getElementById("output").innerHTML = output_val
     }
-}
-
-//user login button
-function loginUser() {
-    document.getElementById("loginForm").style = "display:visible"
-    document.getElementById("addUser").style = "display:none";
-    document.getElementById("deleteUser").style = "display:none";
-    document.getElementById("screen").style = "visibility:hidden";
-
 }
 
 //Add user function button function
@@ -30,48 +25,9 @@ function addUser() {
     document.getElementById("loginForm").style = "display:none"
     document.getElementById("deleteUser").style = "display:none";
     document.getElementById("screen").style = "visibility:hidden";
-
 }
 
-//delete a user
-function deleteUser() {
-    document.getElementById("addUser").style = "display:none";
-    document.getElementById("loginForm").style = "display:none"
-    document.getElementById("deleteUser").style = "display:visible";
-    document.getElementById("screen").style = "visibility:hidden";
-}
-
-
-// function deleteUserForm() 
-document.getElementById("deleteUser").addEventListener("submit",(e)=>{
-    e.preventDefault();
-    let deleteUser_email=document.getElementById("deleteUser_email").value
-    for(let m=0;m<arr.length;m++)
-    {
-        if(arr[m].email===deleteUser_email)
-        {
-            arr.splice(m,1)
-            alert("User  has been deleted")
-            output_val='';
-            generateUser();
-            break;
-        }
-        else
-        alert("User not found")
-    }
-})
-
-
-//user login
-function loginUser() {
-    form = document.getElementById("loginForm")
-    form.style = "display:visible"
-    document.getElementById("addUser").style = "display:none";
-}
-
-
-
-//Add user form
+//ADD USER FORM
 document.getElementById("addUserForm").addEventListener("submit", function (e) {
     e.preventDefault();
 
@@ -88,32 +44,71 @@ document.getElementById("addUserForm").addEventListener("submit", function (e) {
 
 })
 
-//form
+
+
+//DELETE USER SIDE BUTTON
+function deleteUser() {
+    document.getElementById("addUser").style = "display:none";
+    document.getElementById("loginForm").style = "display:none"
+    document.getElementById("deleteUser").style = "display:visible";
+    document.getElementById("screen").style = "visibility:hidden";
+}
+
+//DELETE USER FUNCTION
+document.getElementById("deleteUser").addEventListener("submit", (e) => {
+    e.preventDefault();
+    let deleteUser_email = document.getElementById("deleteUser_email").value
+    for (let m = 0; m < arr.length; m++) {
+        if (arr[m].email === deleteUser_email) {
+            arr.splice(m, 1)
+            alert("User  has been deleted")
+            output_val = '';
+            isDeleted = true;
+            i--;
+            generateUser();
+            break;
+        }
+    }
+    if (isDeleted == false)
+        alert("User not found")
+})
+
+
+//LOGIN FORM SIDE BUTTON
+function loginUser() {
+    document.getElementById("loginForm").style = "display:visible"
+    document.getElementById("addUser").style = "display:none";
+    document.getElementById("deleteUser").style = "display:none";
+    document.getElementById("screen").style = "visibility:hidden";
+
+}
+//LOGIN FORM
 document.getElementById("loginForm").addEventListener("submit", (e) => {
 
     e.preventDefault();
     let inpName = document.getElementById("inp_name").value;
     let inpEmail = document.getElementById("inp_email").value;
     let inpPhone = document.getElementById("inp_phone").value;
-    console.log(inpEmail)
+    // console.log(inpEmail)
+
+    //FORM VELIDATION
+    {
+        if (inpName.charAt(0) == ' ') {
+            alert("Name is not entered yet")
+            document.getElementById("inp_name").style = "outline:2px solid red"
+
+            return false
+
+        }
+
+        if (inpPhone.charAt(0) == ' ' || inpPhone.length < 11) {
+            alert("phone number is not valid")
+            document.getElementById("inp_phone").style = "outline:2px solid red"
+            return false
+
+        }
+    }
     alert(`${inpName} loged in sucessfully`)
 
-
-
-    //  {
-    //      if (inpName.charAt(0) == ' ') {
-    //          alert("Name is not entered yet")
-    //          document.getElementById("inp_name").style = "outline:2px solid red"
-
-    //          return false
-
-    //      }
-
-    //      if (inpPhone.charAt(0) == ' ' || userPhone.length > 13) {
-    //         alert("phone number is not valid")
-    //          document.getElementById("inp_phone").style = "outline:2px solid red"
-
-    //      }
-    //  }
 
 })
